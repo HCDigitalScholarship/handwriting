d851def4ee12378a2d9ffa73d34dccfaafeffa07
Zhanpeng Wang <zwang10@haverford.edu>
2018-04-27 16:39:19 -0400

    the training data with google vision's normalization done.

--- xy_extraction_to_cpkl.py
+++ xy_extraction_to_cpkl.py
@@ -68,32 +68,6 @@ def getStroke(filename):
         result.append(points)
     return result
 
-# the codes from Andy. This normalization is used by Google vision
-# I will adjust it to work with our model.
-def parse_dict(dict):
-
-    class_name = dict["filename"]
-    inkarray = dict["strokes"]
-    stroke_lengths = [len(stroke[0]) for stroke in inkarray]
-    total_points = sum(stroke_lengths)
-    np_ink = np.zeros((total_points, 3), dtype=np.float32)
-    current_t = 0
-    for stroke in inkarray:
-        for i in [0, 1]:
-            np_ink[current_t:(current_t + len(stroke[0])), i] = stroke[i]
-        current_t += len(stroke[0])
-        np_ink[current_t - 1, 2] = 1  # stroke_end
-    # Preprocessing.
-    # 1. Size normalization.
-    lower = np.min(np_ink[:, 0:2], axis=0)
-    upper = np.max(np_ink[:, 0:2], axis=0)
-    scale = upper - lower
-    scale[scale == 0] = 1
-    np_ink[:, 0:2] = (np_ink[:, 0:2] - lower) / scale
-    # 2. Compute deltas.
-    np_ink = np_ink[1:, 0:2] - np_ink[0:-1, 0:2]
-    return np_ink, class_name
-
 # converts a list of arrays into a 2d numpy int16 array
 def convert_stroke_to_array(stroke):
     n_point = 0
@@ -117,4 +91,82 @@ def convert_stroke_to_array(stroke):
             counter += 1
     return stroke_data
 
-process("./lineStrokes","strokes_training_data.cpkl")
+# the codes from Andy. This normalization is used by Google vision
+# I will adjust it to work with our model.
+def parse_dict(dict):
+    if dict is None:
+        return None
+    class_name = dict["filename"]
+    inkarray = dict["strokes"]
+    stroke_lengths = [len(stroke[0]) for stroke in inkarray]
+    total_points = sum(stroke_lengths)
+    np_ink = np.zeros((total_points, 3), dtype=np.float32)
+    current_t = 0
+    for stroke in inkarray:
+        for i in [0, 1]:
+            np_ink[current_t:(current_t + len(stroke[0])), i] = stroke[i]
+        current_t += len(stroke[0])
+        np_ink[current_t - 1, 2] = 1  # stroke_end
+    # Preprocessing.
+    # 1. Size normalization.
+    lower = np.min(np_ink[:, 0:2], axis=0)
+    upper = np.max(np_ink[:, 0:2], axis=0)
+    scale = upper - lower
+    scale[scale == 0] = 1
+    np_ink[:, 0:2] = (np_ink[:, 0:2] - lower) / scale
+    # 2. Compute deltas.
+    np_ink = np_ink[1:, 0:2] - np_ink[0:-1, 0:2]
+    return np_ink, class_name
+
+def create_dictionary_from_xml(xml_file_name):
+    try:
+        dict = {}
+        filename = os.path.basename(xml_file_name).split('.')[0]
+        dict['filename'] = filename
+        tree = ET.parse(xml_file_name)
+        root = tree.getroot()
+        strokes_list = []
+        for i in range(len(root[1])):
+            x_list = []
+            y_list = []
+            stroke = root[1][i]
+            for coordinates in stroke:
+                x_list.append(int(coordinates.attrib['x']))
+                y_list.append(int(coordinates.attrib['y']))
+            
+            stroke_list = [x_list, y_list]
+            strokes_list.append(stroke_list)
+            dict['strokes'] = strokes_list
+        #print(dict)
+        return dict
+    except:
+        return None
+
+# create the training data for the handwriting modeling. The file format is pickle file.
+def create_the_training_data(data_dir, data_file):
+    # built the list of xml files
+    filelist = []
+    # set the data file directory
+    rootDir = data_dir
+    for dirName, subdirList, fileList in os.walk(rootDir):
+        for fname in fileList:
+            filelist.append(dirName + "/" + fname)
+    #print(filelist)
+    
+    strokes = [] # a list of strokes in array
+    for file in filelist:
+        print("Processing ", file)
+        the_strokes_filename_pair = parse_dict(create_dictionary_from_xml(file))
+        if the_strokes_filename_pair is not None:
+            arrays = the_strokes_filename_pair[0] # we only need the strokes array
+            strokes.append(arrays)
+    print(strokes)
+    
+    # dump the training data into a big pickle file.
+    fd = open(data_file, 'a').close()
+    fd = open(data_file, 'wb')
+    pickle.dump(strokes, fd, protocol=2)
+    fd.close()
+
+#process("./lineStrokes","strokes_training_data.cpkl")
+create_the_training_data("./lineStrokes", "strokes_training_data.cpkl")
